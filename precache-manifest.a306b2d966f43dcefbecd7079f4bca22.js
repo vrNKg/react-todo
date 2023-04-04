3e23dbd52116127cb5b7723ba5fa49fb970b272d
vrNKg <veronika.gamalia@mail.ru>
2023-04-04 09:59:40 +0300

    Updates

---
+++ precache-manifest.a306b2d966f43dcefbecd7079f4bca22.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "39f381d825303c34bff5",
+    "url": "/static/css/main.6c7d0e74.chunk.css"
+  },
+  {
+    "revision": "39f381d825303c34bff5",
+    "url": "/static/js/main.39f381d8.chunk.js"
+  },
+  {
+    "revision": "e04cd450e9824253c1bd",
+    "url": "/static/js/1.e04cd450.chunk.js"
+  },
+  {
+    "revision": "66aed819492e1e4035cc",
+    "url": "/static/js/2.66aed819.chunk.js"
+  },
+  {
+    "revision": "71b23b6215511af73d94",
+    "url": "/static/js/runtime~main.71b23b62.js"
+  },
+  {
+    "revision": "28e87e5ee39bfcd2f1158e9f3e609016",
+    "url": "/static/media/delete.28e87e5e.svg"
+  },
+  {
+    "revision": "1be3da344215b126cf44aa555b697e06",
+    "url": "/static/media/edit.1be3da34.svg"
+  },
+  {
+    "revision": "f48ea4dbcf7bf8da229baafc8229d713",
+    "url": "/static/media/accept.f48ea4db.svg"
+  },
+  {
+    "revision": "a9344d12470e5eca790398b770258dfc",
+    "url": "/static/media/remove.a9344d12.svg"
+  },
+  {
+    "revision": "39bb1731c2a6bacb5841965b08cfe7c1",
+    "url": "/index.html"
+  }
+];
